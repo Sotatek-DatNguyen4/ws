23e174ccc66ce7d5cd583e863b748a344348193c
namnt <nam.nguyen10@sotatek.com>
2022-10-27 13:44:42 +0700

    config eslint, prettier, tsconfig, styled

---
+++ src/utils/globalStyleVariable.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primaryBtn: '#9747FF',
+  sencondaryBtn: '#DEE5EA',
+  black: '#333333',
+  red: '#FA0050',
+  noti: '#FFCB2E',
+  alert: '#F36B33',
+}
+
+export const font = {
+  primary: 'Roboto',
+}
